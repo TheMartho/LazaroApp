e4d381401a594c563c9fb27d6a6f97854103a688
unknown <sistemas@VRE_LAB0202.inacap.cl>
2022-06-09 18:46:37 -0400

    DAO Y DTO LISTOS A MEDIAS

--- Fuentes/app/src/main/java/cl/inacap/lazaroapp/dto/Usuario.java
+++ Fuentes/app/src/main/java/cl/inacap/lazaroapp/dto/Usuario.java
@@ -2,7 +2,22 @@ package cl.inacap.lazaroapp.dto;
 
 public abstract class Usuario {
 
-    private String nombre;
-    private String rutPaciente;
+    protected String rut;
+    protected String nombres;
+    protected String apellidos;
+    protected int edad;
+    protected String correo;
+    protected String celular;
+    protected Calificasion calificasion;
+
+public Usuario(String rut, String nombres, String apellidos, int edad, String correo, String celular, Calificasion calificasion){
+    this.rut= rut;
+    this.nombres=nombres;
+    this.apellidos=apellidos;
+    this.edad=edad;
+    this.correo=correo;
+    this.celular=celular;
+    this.calificasion=calificasion;
+}
 
 }
